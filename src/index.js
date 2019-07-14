da04db54c773d57ef5cefda8a6a4003ec89b88a4
Mateusz Mamczarz <mmamczarz@microstrategy.com>
2019-07-14 11:54:22 +0200

    Reorganize code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './roots/Home';
+import Author from './roots/Author';
+import Contact from './roots/Contact';
+import Header from './components/Header';
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Header />
+        <Route exact path="/" component={Home} />
+        <Route path="/author" component={Author} />
+        <Route path="/contact" component={Contact} />
+    </BrowserRouter>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/roots/Home.js
@@ -0,0 +1,15 @@
+import React, { PureComponent } from 'react';
+
+class Home extends PureComponent {
+    fetchPosts
+
+    render() {
+        return (
+            <div>
+                <h2>Posts</h2>
+            </div>
+        )
+    }
+}
+
+export default Home;
