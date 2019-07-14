577e7317636d144feaf3d2fa3d5f6dbbc7273b2e
Mateusz Mamczarz <mmamczarz@microstrategy.com>
2019-07-14 14:16:45 +0200

    Change home page to  function component with hook

--- src/roots/Home.js
+++ src/roots/Home.js
@@ -1,49 +1,34 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
-class Home extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = {
-            posts: [],
+const fetchPosts = (setPosts) => {
+    fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
+        if (response.status === 200) {
+            return response.json();
+        } else {
+            throw new Error(response.statusText);
         }
-        this.fetchPosts();
-    }
+    }).then((posts) => {
+        setPosts(posts);
+    }).catch((error) => {
+        console.error(error);
+    });
+};
 
-    fetchPosts = async () => {
-        await fetch('https://jsonplaceholder.typicode.com/posts').then((response) => {
-            if (response.status === 200) {
-                return response.json();
-            } else {
-                throw new Error(response.statusText);
-            }
-        }).then((posts) => {
-            this.setState({ posts });
-            return posts;
-        }).catch((error) => {
-            console.error(error);
-        });
-    }
+const Home = () => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        fetchPosts(setPosts);
+    }, []);
 
-    renderPosts = () => {
-        const { posts } = this.state;
-        let listOfPosts = [];
-        console.log(posts);
-        posts.forEach((post) => {
-            listOfPosts.push(<div><Link to={`post/${post.id}`}>{post.title}</Link></div>);
-        })
-        return listOfPosts;
-    }
-
-    render() {
-        const { posts } = this.state;
-        return (
-            <div>
-                <h2>Posts</h2>
-                {posts ? this.renderPosts() : <div></div>}
-            </div>
-        )
-    }
+    return (
+        <div>
+            <h2>Posts</h2>
+            <ul>
+                {posts.map((post) => (<li key={post.id}><Link to={`/post/${post.id}`} > {post.title}</Link></li>))}
+            </ul>
+        </div>
+    )
 }
 
 export default Home;
